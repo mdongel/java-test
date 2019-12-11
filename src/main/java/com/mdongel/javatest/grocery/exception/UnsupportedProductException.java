9ac9c36cbbec3cee51cacd7ff291d7066dc4b3c3
MEHMET DONGEL <EXT0232258@turkcell.entp.tgc>
2019-12-12 01:44:39 +0300

    Refactored ShoppingBasketService

---
+++ src/main/java/com/mdongel/javatest/grocery/exception/UnsupportedProductException.java
@@ -0,0 +1,8 @@
+package com.mdongel.javatest.grocery.exception;
+
+public class UnsupportedProductException extends UnsupportedOperationException {
+
+    public UnsupportedProductException(String s) {
+        super(s);
+    }
+}
--- src/test/java/com/mdongel/javatest/grocery/service/ShoppingBasketServiceTest.java
+++ src/test/java/com/mdongel/javatest/grocery/service/ShoppingBasketServiceTest.java
@@ -13,12 +13,12 @@ public class ShoppingBasketServiceTest {
     public void shouldAddProductToShoppingBasket() {
         ShoppingBasketService shoppingBasketService = new ShoppingBasketService();
 
-        shoppingBasketService.addItem(new Item(new Product("soup", Product.Unit.tin, 1.2), 3));
+        shoppingBasketService.addItem("soup", 3);
 
         assertEquals(3, shoppingBasketService.getItems().get("soup").getAmount());
         assertEquals("soup", shoppingBasketService.getItems().get("soup").getProduct().getName());
 
-        assertEquals(1.2, shoppingBasketService.getItems().get("soup").getProduct().getCost(), 0);
+
     }
 
     @Test
@@ -26,8 +26,8 @@ public class ShoppingBasketServiceTest {
         ShoppingBasketService shoppingBasketService = new ShoppingBasketService();
 
         Product soup = new Product("soup", Product.Unit.tin, 1.2);
-        shoppingBasketService.addItem(new Item(soup, 3));
-        shoppingBasketService.addItem(new Item(soup, 8 ));
+        shoppingBasketService.addItem("soup", 3);
+        shoppingBasketService.addItem("soup", 8);
 
 
         assertEquals(11, shoppingBasketService.getItems().get("soup").getAmount());
